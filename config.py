4cf24c0bc258de13cfc0e610273466083fc98f62
DeveloperPrince <princekudzaimaposa94@gmail.com>
2020-01-16 20:27:09 +0200

    changes

--- config.py
+++ config.py
@@ -8,5 +8,5 @@ class DefaultConfig:
     """ Bot Configuration """
 
     PORT = 3978
-    APP_ID = os.environ.get("MicrosoftAppId", "")
-    APP_PASSWORD = os.environ.get("MicrosoftAppPassword", "")
+    APP_ID = os.environ.get("MICROSOFT_APP_ID", "")
+    APP_PASSWORD = os.environ.get("MICROSOFT_APP_PASSWORD", "")
