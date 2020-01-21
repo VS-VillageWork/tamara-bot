7c31ed1270a30bf522360d153864fbed538f7ee6
developerprince <N01416165H@students.nust.ac.zw>
2020-01-21 14:16:58 +0200

    changes Test Integration

---
+++ tests/__init__.py
@@ -0,0 +1,6 @@
+import test_space as test_space
+
+def main():
+    ts_space = test_space()
+    ts_space.test_coworking()
+    pass
---
+++ tests/test_space.py
@@ -0,0 +1,13 @@
+import library.space.main as space
+import unittest
+
+
+class SpaceTest(unittest.TesCase):
+    def test_coworking(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
