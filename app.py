3bbbcbc2f0aa0d270fe8cd7ae1115e38f4cb90e3
DeveloperPrince <princekudzaimaposa94@gmail.com>
2020-01-25 00:02:08 +0200

    Procfile edit

--- app.py
+++ app.py
@@ -83,8 +83,12 @@ async def messages(req: Request) -> Response:
 APP = web.Application(middlewares=[aiohttp_error_middleware])
 APP.router.add_post("/api/messages", messages)
 
-if __name__ == "__main__":
+
+def app():
     try:
         web.run_app(APP, host="localhost", port=CONFIG.PORT)
     except Exception as error:
         raise error
+
+if __name__ == "__main__":
+    app()
